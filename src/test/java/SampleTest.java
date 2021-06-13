47f0a5b460d9118dc3be5b6a948d499497f008a6
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-13 17:39:28 +0300

    ssl

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -3,10 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SampleTest {
-    @Test
-    public void Logo(){
-        WebDriver webDriver = new ChromeDriver();
-        webDriver.get("http://www.youtube.com/watch?v=fOeQWJ1QMSY&ab_channel=HillelITSchool");
+    public static void main(String[]args){
+        System.out.println("HI");
 
     }
 }
