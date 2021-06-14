d84d21b7c297fa3bfe7f9814e9439c61a4e12e3d
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-14 10:24:13 +0300

    ssl

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,10 +1,26 @@
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
+
 public class SampleTest {
-    public static void main(String[]args){
-        System.out.println("HI");
+
+
+    @Test
+    public void MoodleLogin() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
+        webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.MINUTES);
+        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
+        webDriver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("prokopets.yevhen@chnu.edu.ua");
+        webDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button")).click();
+        webDriver.findElement(By.name("password")).sendKeys("Toradora1");
+        webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button")).click();
+
+
 
     }
 }
