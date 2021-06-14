8fc1b20d89e1d8093395b3deb1e1c993fb451d55
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-14 13:06:28 +0300

    ssl

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -20,7 +20,13 @@ public class SampleTest {
         webDriver.findElement(By.name("password")).sendKeys("Toradora1");
         webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button")).click();
 
-
-
     }
+
+    @Test
+    public void Git() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://github.com/yevhen-prokopets");
+        webDriver.findElement(By.xpath("//*[@id=\"js-contribution-activity\"]/div[2]/div/div[2]/div[2]/details/div/ul/li[2]/span[1]/span/a")).click();
+    }
+
 }
