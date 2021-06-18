33e747cf3f9db36ad7e12d153aca31eb0ed57615
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-18 13:19:35 +0300

    ssl

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,7 +1,10 @@
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 import java.util.concurrent.TimeUnit;
 
@@ -28,5 +31,21 @@ public class SampleTest {
         webDriver.get("https://github.com/yevhen-prokopets");
         webDriver.findElement(By.xpath("//*[@id=\"js-contribution-activity\"]/div[2]/div/div[2]/div[2]/details/div/ul/li[2]/span[1]/span/a")).click();
     }
+    @Test
+    public void GitF() {
+        System.setProperty("webdriver.geckodriver.driver", "D:\\firedriver\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://github.com/yevhen-prokopets");
+        driver.findElement(By.xpath("//*[@id=\"js-contribution-activity\"]/div[2]/div/div[2]/div[2]/details/div/ul/li[2]/span[1]/span/a")).click();
+    }
+    @Test
+    public void authenticateUsing() {
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.manage().deleteAllCookies();
+        webDriver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
+        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
 
+    }
 }
