0cf911b84483451db9ee1ecdd9f5a9a8d7e6b9d2
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-23 22:08:38 +0300

    ssl

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -3,7 +3,6 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 import java.util.concurrent.TimeUnit;
