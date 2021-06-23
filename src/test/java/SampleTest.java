3984c0af11aabcaca44be296fc8c46b34e746ea1
yevhen-prokopets <73821875+yevhen-prokopets@users.noreply.github.com>
2021-06-23 21:08:36 +0300

    ssl

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -31,21 +31,7 @@ public class SampleTest {
         webDriver.get("https://github.com/yevhen-prokopets");
         webDriver.findElement(By.xpath("//*[@id=\"js-contribution-activity\"]/div[2]/div/div[2]/div[2]/details/div/ul/li[2]/span[1]/span/a")).click();
     }
-    @Test
-    public void GitF() {
-        System.setProperty("webdriver.geckodriver.driver", "D:\\firedriver\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
-        driver.get("https://github.com/yevhen-prokopets");
-        driver.findElement(By.xpath("//*[@id=\"js-contribution-activity\"]/div[2]/div/div[2]/div[2]/details/div/ul/li[2]/span[1]/span/a")).click();
-    }
-    @Test
-    public void authenticateUsing() {
-        WebDriver webDriver = new FirefoxDriver();
-        webDriver.manage().deleteAllCookies();
-        webDriver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-        webDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
-        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
+
 
     }
-}
+
