47748c2ff66a8692afd03db642b08ca14dae6c3a
yevhen-prokopets <akkermanzena99@gmail.com>
2021-07-21 12:06:16 +0300

    test

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,11 +1,8 @@
 
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-import java.util.concurrent.TimeUnit;
 
 
 public class SampleTest {
@@ -14,14 +11,7 @@ public class SampleTest {
     @Test
     public void MoodleLogin() {
         WebDriver webDriver = new ChromeDriver();
-        webDriver.get("https://moodle.chnu.edu.ua/login/index.php");
-        webDriver.manage().timeouts().implicitlyWait(40, TimeUnit.MINUTES);
-        webDriver.findElement(By.xpath("//*[@id=\"region-main\"]/div/div/div/div/div/div/div[2]/div[1]/div/a")).click();
-        webDriver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("prokopets.yevhen@chnu.edu.ua");
-        webDriver.findElement(By.xpath("//*[@id=\"identifierNext\"]/div/button")).click();
-        webDriver.findElement(By.name("password")).sendKeys("Toradora1");
-        webDriver.findElement(By.xpath("//*[@id=\"passwordNext\"]/div/button")).click();
-
+        webDriver.get("https://moodle.chnu.edu.ua");
     }
 
     }
